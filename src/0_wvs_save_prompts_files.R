feb21a517b70e8a3c3b51749b2fac3d3dd489c76
gefero <german.rosati@gmail.com>
2025-07-07 16:12:47 -0300

    Agrego bases para prompts

--- src/0_wvs_save_prompts_files.R
+++ src/0_wvs_save_prompts_files.R
@@ -1,17 +1,47 @@
 library(tidyverse)
-library(jsonlite)
 #load('./data/wvs/df_prompt.RData')
 load('./data/wvs/df_long.RData')
 
 df_long %>%
-        select(id,D_INTERVIEW, B_COUNTRY_ALPHA, Country_label, dependiente, full_prompt) %>%
+        rename(Age = Q262) %>%
+        select(id,D_INTERVIEW, B_COUNTRY_ALPHA, Country_label, 
+               Age, Sex_label:Occupation_label, dependiente, question_text) %>%
         group_split(dependiente) %>% 
         purrr::walk(function(df_long) {
                 nombre_grupo <- unique(df_long$dependiente)
-                write_csv(df_long, paste0("./data/wvs/prompts_", nombre_grupo, ".csv"))
+                write_csv(df_long, paste0("./data/wvs/", nombre_grupo, ".csv"))
+        }
+        )
+        
+        
+
+
+set.seed(123)
+df_long %>%
+        select(id,D_INTERVIEW, B_COUNTRY_ALPHA, Country_label, dependiente, full_prompt) %>%
+        group_by(Country_label, dependiente) %>%
+        sample_n(5) %>%
+        ungroup() %>%
+        group_split(dependiente) %>% 
+        purrr::walk(function(df_long) {
+                nombre_grupo <- unique(df_long$dependiente)
+                write_csv(df_long, paste0("./data/wvs/sample_prompts_", nombre_grupo, ".csv"))
         }
         )
 
+
+df_long %>%
+        filter(
+                Country_label %in% c("Argentina","United States", "Uruguay","United Kingdom") & 
+                        dependiente == "Q199") %>%
+        group_by(Country_label) %>%
+        summarise(n=n())
+
+
+df_long %>%
+        sample_n(10) %>%
+        select(full_prompt) %>%
+        pull()
 
 x <- read_csv('./data/wvs/prompts_Q199.csv')
 
